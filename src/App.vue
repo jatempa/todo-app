6aa85df6e1548213b3cd42e9de5f4ce9b6fc2d98
Jorge Atempa <atempaitm@gmail.com>
2023-01-05 15:11:45 -0800

    Create Card component

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,30 @@
-<script setup>
+<script>
+import Card from './components/Card.vue';
 import Header from './components/Header.vue';
 import Input from './components/Input.vue';
+
+export default {
+  components: {
+    Card,
+    Header,
+    Input,
+  },
+  data() {
+    return {
+      message: 'x',
+    };
+  },
+  methods: {
+    handleUpdate(item) {
+      console.log(item);
+    },
+  },
+};
 </script>
 
 <template>
-  <Header title="Todo List" />
-  <Input />
+  <Card>
+    <Header title="Todo List" />
+    <Input v-model="message" @onChangeInput="handleUpdate" />
+  </Card>
 </template>
