d351c1795de9bf6e6e00d034bdfb21b525f6338a
Jorge Atempa <atempaitm@gmail.com>
2023-01-05 10:26:21 -0800

    Create Input component

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <script setup>
 import Header from './components/Header.vue';
+import Input from './components/Input.vue';
 </script>
 
 <template>
   <Header title="Todo List" />
+  <Input />
 </template>
---
+++ src/components/Input.vue
@@ -0,0 +1,16 @@
+<script>
+export default {};
+</script>
+
+<template>
+  <input v-model="item" type="text" />
+</template>
+
+<style scoped>
+input {
+  font-size: 2rem;
+  outline: none;
+  line-height: 8px;
+  vertical-align: baseline;
+}
+</style>
