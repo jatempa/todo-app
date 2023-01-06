1a1fbb02bc1e7430312abc5f1bd0629710d7ba8c
Jorge Atempa <atempaitm@gmail.com>
2023-01-05 23:13:38 -0800

    Create component to show tasks by status amount

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,10 @@
 <script>
+import { v4 as uuidv4 } from 'uuid';
 import Card from './components/Card.vue';
 import Header from './components/Header.vue';
 import CustomTaskInput from './components/CustomTaskInput.vue';
 import ItemList from './components/ItemList.vue';
-import { v4 as uuidv4 } from 'uuid';
+import Results from './components/Results.vue';
 
 export default {
   components: {
@@ -11,6 +12,7 @@ export default {
     CustomTaskInput,
     Header,
     ItemList,
+    Results,
   },
   data() {
     return {
@@ -30,6 +32,18 @@ export default {
       this.items.push(newItem);
     },
   },
+  computed: {
+    tasksByStatus() {
+      const complete = this.items.filter((item) => item.done).length;
+      const incomplete = this.items.length - complete;
+
+      return {
+        all: this.items.length,
+        complete,
+        incomplete,
+      };
+    },
+  },
 };
 </script>
 
@@ -38,5 +52,6 @@ export default {
     <Header>To Do</Header>
     <CustomTaskInput @handle-change="handleChange" />
     <ItemList :items="items" />
+    <Results :tasks="tasksByStatus" />
   </Card>
 </template>
---
+++ src/components/Results.vue
@@ -0,0 +1,28 @@
+<script>
+export default {
+  props: {
+    tasks: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
+
+<template>
+  <div v-if="tasks.all > 0">
+    <hr />
+    <div class="container">
+      <span><strong>Total:</strong> {{ tasks.all }}</span>
+      <span><strong>Complete:</strong> {{ tasks.complete }}</span>
+      <span><strong>Incomplete:</strong> {{ tasks.incomplete }}</span>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  display: flex;
+  justify-content: space-evenly;
+}
+</style>
