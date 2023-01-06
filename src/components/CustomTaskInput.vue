54dfee2e61870c0f4c0ba82153dcb2b475c29d3d
Jorge Atempa <atempaitm@gmail.com>
2023-01-05 19:52:53 -0800

    Remove 'outline' property

--- src/components/CustomTaskInput.vue
+++ src/components/CustomTaskInput.vue
@@ -18,12 +18,20 @@ export default {
     :value="task"
     @input="task = $event.target.value"
     @keyup.enter="addTask"
+    placeholder="Enter your task ..."
   />
 </template>
 
 <style scoped>
 input {
+  width: 100%;
   font-size: 2rem;
+  padding: 0px;
+  border: 0px;
   outline: none;
 }
+
+/* input:focus {
+  outline-color: blue;
+} */
 </style>
