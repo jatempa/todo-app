8e708c668a53d4202a1f6e3314a70d96f95ac7d1
Jorge Atempa <atempaitm@gmail.com>
2023-01-18 12:58:02 -0800

    Update 'handleChange' method

--- src/App.vue
+++ src/App.vue
@@ -2,14 +2,14 @@
 import { v4 as uuidv4 } from 'uuid';
 import Card from './components/Card.vue';
 import Header from './components/Header.vue';
-import CustomTaskInput from './components/CustomTaskInput.vue';
+import CustomInput from './components/CustomInput.vue';
 import ItemList from './components/ItemList.vue';
 import Results from './components/Results.vue';
 
 export default {
   components: {
     Card,
-    CustomTaskInput,
+    CustomInput,
     Header,
     ItemList,
     Results,
@@ -17,19 +17,21 @@ export default {
   data() {
     return {
       items: [],
+      task: '',
     };
   },
   methods: {
-    handleChange(item) {
-      if (!item || item.length === 0) return;
+    handleChange() {
+      if (this.task.length === 0) return;
 
       const newItem = {
         id: uuidv4(),
-        title: item,
+        title: this.task,
         done: false,
       };
 
       this.items.push(newItem);
+      this.task = '';
     },
   },
   computed: {
@@ -50,7 +52,7 @@ export default {
 <template>
   <Card>
     <Header>To Do</Header>
-    <CustomTaskInput @handle-change="handleChange" />
+    <CustomInput v-model="task" @handle-change="handleChange" />
     <ItemList :items="items" />
     <Results :tasks="tasksByStatus" />
   </Card>
