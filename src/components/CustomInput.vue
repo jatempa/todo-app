a60eeba8b7edcf5093f0b203afe1d4a77e0d6191
Jorge Atempa <atempaitm@gmail.com>
2023-01-18 12:53:56 -0800

    Update 'CustomInput' component

---
+++ src/components/CustomInput.vue
@@ -0,0 +1,25 @@
+<script>
+export default {
+  props: ['modelValue'],
+  emits: ['update:modelValue', 'handle-change'],
+};
+</script>
+
+<template>
+  <input
+    :value="modelValue"
+    @input="$emit('update:modelValue', $event.target.value)"
+    @keyup.enter="$emit('handle-change')"
+    placeholder="Enter your task ..."
+  />
+</template>
+
+<style scoped>
+input {
+  width: 100%;
+  font-size: 2rem;
+  padding: 0px;
+  border: 0px;
+  outline: none;
+}
+</style>
